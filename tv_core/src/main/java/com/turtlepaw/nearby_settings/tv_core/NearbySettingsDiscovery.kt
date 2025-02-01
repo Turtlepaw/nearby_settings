c5b83344db85dc8188d34df8967dde4731c8f9dc
Turtlepaw <81275769+Turtlepaw@users.noreply.github.com>
2025-01-31 22:10:25 -0500

    refactor: rename to NearbySettingsDiscovery

--- tv_core/src/main/java/com/turtlepaw/nearby_settings/tv_core/NearbySettingsDiscovery.kt
+++ tv_core/src/main/java/com/turtlepaw/nearby_settings/tv_core/NearbySettingsDiscovery.kt
@@ -7,15 +7,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -25,11 +21,8 @@ import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
-import androidx.compose.ui.window.DialogProperties
 import androidx.core.content.ContextCompat
 import androidx.tv.material3.MaterialTheme
-import androidx.tv.material3.Surface
 import androidx.tv.material3.Text
 import com.github.alexzhirkevich.customqrgenerator.QrData
 import com.github.alexzhirkevich.customqrgenerator.vector.QrCodeDrawable
@@ -47,7 +40,7 @@ import com.github.alexzhirkevich.customqrgenerator.vector.style.QrVectorShapes
 import com.google.accompanist.drawablepainter.rememberDrawablePainter
 
 @Composable
-fun DiscoveryDialog() {
+fun NearbySettingsDiscovery() {
     val data = QrData.Url("https://nearbysettings.pages.dev/users")
     val options = QrVectorOptions.Builder()
         .setPadding(0f)
@@ -137,6 +130,6 @@ private fun DiscoveryDialogPreview(){
             )
             .clip(shape = MaterialTheme.shapes.medium)
     ) {
-        DiscoveryDialog()
+        NearbySettingsDiscovery()
     }
 }
